580ca33abeb0e0b5812fa7343772a5f7206762dc
vika Barbara <djakovololo@gmail.com>
2023-01-03 14:09:31 +0600

    Initial commit

---
+++ dz1.py
@@ -0,0 +1,9 @@
+money = int(input("Введите сумму вклада"))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit=[]
+deposit.append(int(per_cent["ТКБ"]*money/100))
+deposit.append(int(per_cent["СКБ"]*money/100))
+deposit.append(int(per_cent["ВТБ"]*money/100))
+deposit.append(int(per_cent["СБЕР"]*money/100))
+print("Hакопленные за год средства в каждом из банков", deposit)
+print("Максимальная сумма, которую вы можете заработать", max(deposit))
